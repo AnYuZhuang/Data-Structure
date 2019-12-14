fe67a60e638067285f543e490c659a90fffbd1ab
AnYuZhuang <40970101+AnYuZhuang@users.noreply.github.com>
2019-12-15 00:38:05 +0800

    Create merge sort.cpp

---
+++ merge sort.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int total;
+    cin>>total;
+    int arr[total];
+
+    for(int i = 0;i < total;i++){
+        int num;
+        cin>>num;
+        arr[i] = num;
+    }
+    
+    
+}
