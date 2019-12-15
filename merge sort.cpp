10cb1b77a9581391fa345f3dc70400ad3b756f99
AnYuZhuang <40970101+AnYuZhuang@users.noreply.github.com>
2019-12-16 00:49:44 +0800

    Update merge sort.cpp

--- merge sort.cpp
+++ merge sort.cpp
@@ -1,11 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void MergeSort(int *arr,const int n){
-    int *tempList = new int[n + 1];
+//merge two subArr.
+void Merge(int *arr,int m,int lef,int rig){
+    int l = m - lef + 1;//left arr size.
+    int r = rig - m;//right arr size.
+    int Left[l];
+    int Right[r];
+    for(int i = 0;i < l;i++){//copy data to new left arr.
+        Left[i] = arr[l + i];
+    }
+    for(int j = 0;j < r;j++){//copy data to new right arr.
+        Right[j] = arr[m + 1 + j];
+    }
+    int idx_l = 0;
+    int idx_r = 0;
+    int idx_mArr = lef;//index of mergedArr.
+    while(idx_l < l && idx_r < r){
+        if(Left[idx_l] <= Right[idx_r]){
+            arr[idx_mArr] = Left[idx_l];
+            idx_l++;
+        }
+        else{
+            arr[idx_mArr] = Right[idx_r];
+            idx_r++;
+        }
+        idx_mArr++;
+    }
+    while(idx_l < l){
+        arr[idx_mArr] = Left[idx_l];
+        idx_l++;
+        idx_mArr++;
+    }
+    while(idx_r < r){
+        arr[idx_mArr] = Right[idx_r];
+        idx_r++;
+        idx_mArr++;
+    }
+
+}
+
+//divide arr.
+void MergeSort(int *arr,int left,int right){
+    if(left < right){
+        int middle = left+(right - left)/2;//?
+        MergeSort(arr,left,middle);
+        MergeSort(arr,middle+1,right);
+        Merge(arr,middle,left,right);
+
+    }
 
 
-    delete [] tempList;
+
 }
 
 int main()
@@ -19,10 +65,11 @@ int main()
         cin>>num;
         arr[i] = num;
     }
-    MergeSort(arr,total);
-    /*
+    MergeSort(arr,0,total-1);
+
     //Output arr for checking
     for(int i = 0;i < total;i++)
         cout<<arr[i]<<' ';
-    */
+
+    return 0;
 }
